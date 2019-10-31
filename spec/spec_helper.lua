953f970c9b065ca574f8635e3a411ba2aa6c88c3
David Balatero <dbalatero@gmail.com>
2019-10-31 14:15:18 -0700

    Add logging and inspect to specs

--- spec/spec_helper.lua
+++ spec/spec_helper.lua
@@ -1,1 +1,10 @@
+inspect = dofile("lib/utils/inspect.lua")
+
 vimModeScriptPath = ""
+
+vimLogger = dofile("lib/utils/log.lua")
+-- conform to the Hammerspoon logging API as well
+vimLogger.i = vimLogger.info
+vimLogger.d = vimLogger.debug
+vimLogger.e = vimLogger.error
+vimLogger.v = vimLogger.trace
