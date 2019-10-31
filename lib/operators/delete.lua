96bb5e992d66eb3cc50206a171f8d568dd5c4bdf
David Balatero <dbalatero@gmail.com>
2019-10-31 14:44:35 -0700

    Have operators return a new buffer

--- lib/operators/delete.lua
+++ lib/operators/delete.lua
@@ -1,5 +1,23 @@
+local Buffer = dofile(vimModeScriptPath .. "lib/buffer.lua")
 local Operator = dofile(vimModeScriptPath .. "lib/operator.lua")
+local Selection = dofile(vimModeScriptPath .. "lib/selection.lua")
+
 local Delete = Operator:new()
+
+function Delete.getModifiedBuffer(buffer, rangeStart, rangeFinish)
+  local stringStart, stringFinish = rangeStart + 1, rangeFinish + 1
+  local contents = ""
+
+  if stringStart > 1 then
+    contents = string.sub(buffer.contents, 1, stringStart - 1)
+  end
+
+  contents = contents .. string.sub(buffer.contents, stringFinish + 1, -1)
+
+  local selection = Selection:new(rangeStart, 0)
+
+  return Buffer:new(contents, selection)
+end
 
 function Delete.getKeys()
   return {
