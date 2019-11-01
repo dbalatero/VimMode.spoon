a56720ff9c545db8f9270a7ce8c8216af69de569
David Balatero <dbalatero@gmail.com>
2019-10-31 21:07:42 -0700

    Add special case comment to change

--- lib/operators/change.lua
+++ lib/operators/change.lua
@@ -2,6 +2,8 @@ local Delete = dofile(vimModeScriptPath .. "lib/operators/delete.lua")
 
 local Change = Delete:new{ name = 'change' }
 
+-- Special case: "cw" and "cW" are treated like "ce" and "cE" if the cursor is
+-- on a non-blank.  This is Vi-compatible, see |cpo-_| to change the behavior.
 function Change.getModeForTransition()
   return "insert"
 end
