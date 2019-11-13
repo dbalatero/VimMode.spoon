19993ff467e9d2157fbcf22cf56636b06ba4f876
David Balatero <dbalatero@gmail.com>
2019-11-12 21:57:18 -0800

    Add yank

---
+++ lib/operators/yank.lua
@@ -0,0 +1,26 @@
+local Operator = dofile(vimModeScriptPath .. "lib/operator.lua")
+local Yank = Operator:new{name = 'yank'}
+
+function Yank.getModifiedBuffer(buffer, rangeStart, rangeFinish)
+  -- we just want to set it in the pasteboard
+  if hs then
+    local stringStart, stringFinish = rangeStart + 1, rangeFinish + 1
+    local toCopy = string.sub(buffer:getValue(), stringStart, stringFinish)
+
+    hs.pasteboard.setContents(toCopy)
+  end
+
+  -- we actually don't need to modify the buffer
+  return buffer
+end
+
+function Yank.getKeys()
+  return {
+    {
+      modifiers = {'cmd'},
+      key = 'c'
+    }
+  }
+end
+
+return Yank
