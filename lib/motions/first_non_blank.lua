43d107b85ca475993b6bf81f6274197a06b3ed94
David Balatero <dbalatero@gmail.com>
2019-11-21 14:24:49 -0800

    Add shift+i for jumping to first non blank and editing (#15)

---
+++ lib/motions/first_non_blank.lua
@@ -0,0 +1,37 @@
+local Motion = dofile(vimModeScriptPath .. "lib/motion.lua")
+local stringUtils = dofile(vimModeScriptPath .. "lib/utils/string_utils.lua")
+
+local FirstNonBlank = Motion:new{ name = 'first_non_blank' }
+
+function FirstNonBlank.getRange(_, buffer)
+  local start = buffer:getSelectionRange():positionEnd()
+  local bufferLength = buffer:getLength()
+  local contents = buffer:getValue()
+
+  local range = {
+    start = start,
+    mode = 'exclusive',
+    direction = 'characterwise'
+  }
+
+  range.finish = start
+
+  while range.finish < bufferLength do
+    local charIndex = range.finish + 1 -- lua strings are 1-indexed :(
+    local char = string.sub(contents, charIndex, charIndex)
+
+    if char == "\n" then break end
+    if not stringUtils.isWhitespace(char) then break end
+
+    range.finish = range.finish + 1
+  end
+
+  return range
+end
+
+-- TODO not possible without context
+function FirstNonBlank.getMovements()
+  return {}
+end
+
+return FirstNonBlank
