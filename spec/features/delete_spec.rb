25e17ad0f94117a1481489b618346b71fef20c55
David Balatero <dbalatero@gmail.com>
2019-12-08 11:06:20 -0800

    Add feature spec

---
+++ spec/features/delete_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'deletion', type: :request, js: true do
+  before { open_and_focus_page! }
+
+  context 'words' do
+    it 'deletes a single word value' do
+      set_textarea_value_and_selection('|Word')
+      send_normal_mode_keys('dw')
+
+      expect_textarea_to_have_value_and_selection('|')
+    end
+
+    it 'deletes from the middle of a word' do
+      set_textarea_value_and_selection('W|ord')
+      send_normal_mode_keys('dw')
+
+      expect_textarea_to_have_value_and_selection('W|')
+    end
+
+    it 'handles multiple words' do
+      set_textarea_value_and_selection('|Word another')
+
+      send_normal_mode_keys('dw')
+
+      expect_textarea_to_have_value_and_selection('|another')
+    end
+  end
+end
