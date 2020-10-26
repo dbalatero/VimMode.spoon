3c38451efb76ade2db47942969d367209849a41b
David Balatero <dbalatero@gmail.com>
2020-10-26 09:49:24 -0400

    Load luautf8 from luarocks first, otherwise fallback to the vendored version

--- vendor/luautf8.lua
+++ vendor/luautf8.lua
@@ -1,3 +1,6 @@
-local luautf8 = require("luautf8.lua-utf8")
+local prequire = dofile(vimModeScriptPath .. "lib/utils/prequire.lua")
+
+-- Try to load it from luarocks, otherwise require the vendored version.
+local luautf8 = prequire("lua-utf8") or require("luautf8.lua-utf8")
 
 return luautf8
