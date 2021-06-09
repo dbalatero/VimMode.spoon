83f04a65d143e5abbbe68bd3ac1c9edc9937422c
David Balatero <dbalatero@gmail.com>
2021-06-08 23:42:45 -0700

    Update x86 .so, detect arch

--- vendor/luautf8.lua
+++ vendor/luautf8.lua
@@ -1,6 +1,16 @@
 local prequire = dofile(vimModeScriptPath .. "lib/utils/prequire.lua")
 
+local function loadVendored()
+  local arch = hs.execute("uname -p")
+
+  if arch == "arm\n" then
+    return require("luautf8.lua-utf8-arm")
+  else
+    return require("luautf8.lua-utf8-x86")
+  end
+end
+
 -- Try to load it from luarocks, otherwise require the vendored version.
-local luautf8 = prequire("lua-utf8") or require("luautf8.lua-utf8")
+local luautf8 = prequire("lua-utf8") or loadVendored()
 
 return luautf8
