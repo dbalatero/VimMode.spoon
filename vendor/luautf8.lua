c93e518920bd99da776e7243d223e88a706b7fa8
David Balatero <dbalatero@gmail.com>
2021-06-18 15:16:15 -0400

    Add M1 support (#82)

--- vendor/luautf8.lua
+++ vendor/luautf8.lua
@@ -1,6 +1,16 @@
 local prequire = dofile(vimModeScriptPath .. "lib/utils/prequire.lua")
 
+local function loadVendored()
+  local arch = hs.execute("uname -p")
+
+  if arch == "arm\n" then
+    return require("luautf8.arm.lua-utf8")
+  else
+    return require("luautf8.x86.lua-utf8")
+  end
+end
+
 -- Try to load it from luarocks, otherwise require the vendored version.
-local luautf8 = prequire("lua-utf8") or require("luautf8.lua-utf8")
+local luautf8 = prequire("lua-utf8") or loadVendored()
 
 return luautf8
