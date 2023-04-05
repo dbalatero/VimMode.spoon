4fefc226326556caca7c5b6ec9084f8fcca880f8
levabala <levabala@ya.ru>
2023-04-05 16:00:23 +0200

    test(motions): add tests for inline search (f/F/t/T)

---
+++ spec/motions/till_after_search_spec.lua
@@ -0,0 +1,80 @@
+local Buffer = require("lib/buffer")
+local TillAfterSearch = require("lib/motions/till_after_search")
+
+describe("TillAfterSearch", function()
+  it("has a name", function()
+    assert.are.equals("till_after_search", TillAfterSearch:new().name)
+  end)
+
+  describe("#getRange", function()
+    local cases = {
+      {
+        name = "after prev char from the end of the line",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "                          x",
+        to   = "                        x  ",
+      },
+      {
+        name = "after prev char from the middle of the line",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "        x                  ",
+        to   = "      x                    ",
+      },
+      {
+        name = "same char if target is placed right after cursor",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "          x                ",
+        to   = "          x                ",
+      },
+      {
+        name = "skips char if placed on it",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "                   x       ",
+        to   = "          x                ",
+      },
+    }
+
+    for _, case in ipairs(cases) do
+      it(case.name, function()
+        local buffer = Buffer:new()
+        buffer:setValue(case.str)
+
+        local indexFromRaw = case.from:find("x")
+        local indexFrom = indexFromRaw - 1
+        buffer:setSelectionRange(0, indexFrom)
+
+        local tillAfterSearch = TillAfterSearch:new()
+        tillAfterSearch:setExtraChar(case.char)
+
+        local indexToRaw = case.to:find("x")
+        local indexTo = indexToRaw - 1
+        assert.are.same(
+          {
+            start = indexTo,
+            finish = indexFrom,
+            mode = "exclusive",
+            direction = "characterwise"
+          },
+          tillAfterSearch:getRange(buffer),
+          case.name
+        )
+      end)
+    end
+
+    it("does nothing of no occurence found", function()
+      local buffer = Buffer:new()
+      buffer:setValue("just a word")
+
+      buffer:setSelectionRange(7, 0)
+
+      local tillAfterSearch = TillAfterSearch:new()
+      tillAfterSearch:setExtraChar("d")
+
+      assert.is_nil(tillAfterSearch:getRange(buffer))
+    end)
+  end)
+end)
---
+++ spec/motions/till_before_search_spec.lua
@@ -0,0 +1,79 @@
+local Buffer = require("lib/buffer")
+local TillBeforeSearch = require("lib/motions/till_before_search")
+
+describe("TillBeforeSearch", function()
+  it("has a name", function()
+    assert.are.equals("till_before_search", TillBeforeSearch:new().name)
+  end)
+
+  describe("#getRange", function()
+    local cases = {
+      {
+        name = "pre next char from the start of the line",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "x                          ",
+        to   = "    x                      ",
+      },
+      {
+        name = "pre next char from the middle of the line",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "      x                    ",
+        to   = "        x                  ",
+      },
+      {
+        name = "same char if target is placed right after cursor",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "        x                  ",
+        to   = "        x                  ",
+      },
+      {
+        name = "skips char if placed on it",
+        char = "o",
+        str  = "cat dog mouse cat dog mouse",
+        from = "         x                 ",
+        to   = "                  x        ",
+      },
+    }
+
+    for _, case in ipairs(cases) do
+      it(case.name, function()
+        local buffer = Buffer:new()
+        buffer:setValue(case.str)
+
+        local indexFromRaw = case.from:find("x")
+        local indexFrom = indexFromRaw - 1
+        buffer:setSelectionRange(indexFrom, 0)
+
+        local tillBeforeSearch = TillBeforeSearch:new()
+        tillBeforeSearch:setExtraChar(case.char)
+
+        local indexToRaw = case.to:find("x")
+        local indexTo = indexToRaw - 1
+        assert.are.same(
+          {
+            start = indexFrom,
+            finish = indexTo,
+            mode = "inclusive",
+            direction = "characterwise"
+          },
+          tillBeforeSearch:getRange(buffer)
+        )
+      end)
+    end
+
+    it("does nothing of no occurence found", function()
+      local buffer = Buffer:new()
+      buffer:setValue("just a word")
+
+      buffer:setSelectionRange(2, 0)
+
+      local tillBeforeSearch = TillBeforeSearch:new()
+      tillBeforeSearch:setExtraChar("j")
+
+      assert.is_nil(tillBeforeSearch:getRange(buffer))
+    end)
+  end)
+end)
